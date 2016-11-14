4e1f1894835b01676ac2c01a4accdeea56c7ccbb
Adrian <adriancan21@gmail.com>
2016-11-14 01:30:57 +0100

    Finished

--- scripts.js
+++ scripts.js
@@ -5,4 +5,22 @@ app.controller('peopleController', function($scope) {
   {name:"Adri",surname:"Canas",phone:"666666666"}
 
  ];
+
+$scope.Save=function(){
+	$scope.people.push({
+
+		name:$scope.newContact.name, surname:$scope.newContact.surname, phone:$scope.newContact.phone
+		
+	})
+
+	$scope.formVisibility=false;
+	}
+
+	$scope.formVisibility=false;
+
+	$scope.showForm=function(){
+		$scope.formVisibility=true;
+}
+
+
  });
